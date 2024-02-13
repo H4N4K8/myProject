6abd4c7770d10087b633505d57641084ad59e417
H4N4K8 <hannah.francis03@gmail.com>
2024-02-12 20:07:08 -0500

    Add project files.

---
+++ myProject/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace gameStore
+{
+    class game
+    {
+        private string _Name;
+        private string _Console;
+        private string _Type;
+        public game()
+        {
+            _Name = "";
+            _Console = "";
+            _Type = "";
+        }
+        public game(string name, string console, string type)
+        {
+            _Name = name;
+            _Console = console;
+            _Type = type;
+        }
+        public string getName()
+        {
+            return _Name;
+        }
+        public string getConsole()
+        {
+            return _Console;
+        }
+        public string getType()
+        {
+            return _Type;
+        }
+        public void setName(string name)
+        {
+            _Name = name;
+        }
+        public void setConsole(string console)
+        {
+            _Console = console;
+        }
+        public void setType(string type)
+        {
+            _Type = type;
+        }
+    }
+    class myStore
+    {
+
+        static void Main(string[] args)
+        {
+            game game1 = new game();
+            game1.setName("Final Fantasy");
+            game1.setConsole("All");
+            game1.setType("Action");
+
+            game game2 = new game();
+            Console.WriteLine("Please enter the game name:");
+            game2.setName(Console.ReadLine());
+            Console.WriteLine("Please enter the console:");
+            game2.setConsole(Console.ReadLine());
+            Console.WriteLine("Please enter the type:");
+            game2.setType(Console.ReadLine());
+
+            game game3 = new game("Horizon: Forbidden West", "Playstaion", "Adventure");
+
+            Console.WriteLine("Please enter the game name: ");
+            string tempName = (Console.ReadLine());
+            Console.WriteLine("Please enter the console: ");
+            string tempConsole = Console.ReadLine();
+            Console.WriteLine("Please enter the type: ");
+            string tempType = Console.ReadLine();
+            game game4 = new game(tempName, tempConsole, tempType);
+
+            displayGames(game1);
+            displayGames(game2);
+            displayGames(game3);
+            displayGames(game4);
+        }
+        static void displayGames(game game1)
+        {
+            Console.WriteLine("Here's your game information");
+            Console.WriteLine($"Name: {game1.getName()}");
+            Console.WriteLine($"Console: {game1.getConsole()}");
+            Console.WriteLine($"Type: {game1.getType()}");
+        }
+    }
+}
